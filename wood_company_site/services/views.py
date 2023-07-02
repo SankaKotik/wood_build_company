ff6a68d649cf758cbf9fe004b01eb588f58000c5
SankaKotik <56644130+SankaKotik@users.noreply.github.com>
2023-07-02 21:45:17 +0300

    Delete wood_company_site directory

--- wood_company_site/services/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from .models import Service
-
-
-# Create your views here.
-def index(request):
-    cards = Service.objects.all()
-    return render(request, 'services/usl.html', {'data': cards})
